7f9c83f1a96670a42410d30859de40ee746da6e3
mikekoser <mikek@hairdirect.com>
2014-03-07 15:39:52 -0500

    Moved custom javascript to external file

---
+++ js/checkout.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+	var $cta_bar = $('.cta_bar'),
+		footertop = $('#help-content').position().top;
+
+	$(window).on("scroll", function (event) {
+		var scrollPos = window.scrollY,
+			windowheight = window.innerHeight,
+			scrollbottom = scrollPos + windowheight,
+			ctabarheight = $cta_bar.height();
+
+		if (scrollbottom >= footertop) {
+			if (!$cta_bar.hasClass('absolute')) {
+				$cta_bar.addClass('absolute').css({
+					"top": footertop - ctabarheight + "px"
+				});
+			}
+		}
+		else {
+			if ($cta_bar.hasClass('absolute')) {
+				$cta_bar.removeClass('absolute').css({
+					"top": "auto"
+				});
+			}
+		}
+	});
+});
