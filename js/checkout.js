9b7fa7863e2da39de1c67acf4a6e008548cb4433
mikekoser <mikek@hairdirect.com>
2014-03-11 14:37:00 -0400

    interaction development

--- js/checkout.js
+++ js/checkout.js
@@ -1,4 +1,5 @@
-﻿/********** PLUGINS **********/
+﻿//#region -- PLUGINS --
+
 (function ($) {
 	$.fn.toggleContainer = function (options) {
 		// Default options
@@ -11,20 +12,21 @@
 				hide_class: "hide",
 				firing_events: "click",
 				toggle_self: true,
-				toggle_order: "before" /* before|after */
+				toggle_order: "before", /* before|after */
+				self_toggle_delay_offset: 100
 			}, options);
 
 		if (settings.content_element !== undefined) {
 			this.on(settings.firing_events, function () {
 				// Toggle self (before)
 				if (settings.toggle_self && settings.toggle_order == "before") {
-					$self.fadeToggle(settings.delay - 100);
+					$self.fadeToggle(settings.delay + settings.self_toggle_delay_offset);
 				}
 				// Toggle continer
 				settings.content_element.slideToggle(settings.delay);
 				// Toggle self (after)
 				if (settings.toggle_self && settings.toggle_order == "after") {
-					$self.fadeToggle(settings.delay + 100);
+					$self.fadeToggle(settings.delay + settings.self_toggle_delay_offset);
 				}
 				// Fire Callback
 				if (settings.callback !== undefined) {
@@ -36,9 +38,12 @@
 		return this;
 	};
 }(jQuery));
-/********** GLOBAL VARIABLES **********/
+//#endregion PLUGINS
+
+//#region -- GLOBAL VARIABLES --
 var $cta_bar,
 	$help_content,
+	$shipping_address_instructions,
 	$btnchangeaddress,
 	$btncreateaddress,
 	$default_address_wrapper,
@@ -47,7 +52,9 @@ var $cta_bar,
 	footertop,
 	absoluteClassName = "absolute";
 
-/********** FUNCTIONS **********/
+//#endregion -- GLOBAL VARIABLES --
+
+//#region -- FUNCTIONS --
 
 $(document).ready(function () {
 	SetGlobalVariables();
@@ -58,6 +65,7 @@ function SetGlobalVariables() {
 	/// <summary>Gets DOM elements & other dynamic variable values.</summary>
 	$cta_bar = $(".cta_bar");
 	$help_content = $("#help-content");
+	$shipping_address_instructions = $("#shipping-address .instructions");
 	$btnchangeaddress = $("button.changeaddress");
 	$btncreateaddress = $("button.createaddress");
 	$default_address_wrapper = $("div.defaultaddress-wrapper");
@@ -68,15 +76,25 @@ function SetGlobalVariables() {
 function WireEvents() {
 	/// <summary>Wire up control events</summary>
 	$btnchangeaddress.toggleContainer({
-		content_element: $default_address_wrapper,
-		toggle_self: false
-	}).toggleContainer({
-		content_element: $additional_addresses
+		content_element: $additional_addresses,
+		callback: ToggleShippingInstructionText,
+		self_toggle_delay_offset: -300
 	});
 	$btncreateaddress.toggleContainer({
-		content_element: $default_address_wrapper,
-		toggle_self: false
-	}).toggleContainer({
-		content_element: $new_address
+		content_element: $new_address,
+		self_toggle_delay_offset: -300
 	});
 }
+
+function ToggleShippingInstructionText() {
+	var defaultText = $shipping_address_instructions.attr("data-text"),
+		altText = $shipping_address_instructions.attr("data-alt-text");
+	if ($shipping_address_instructions.text().trim() == defaultText) {
+		$shipping_address_instructions.text(altText);
+	}
+	else {
+		$shipping_address_instructions.text(defaultText);
+	}
+}
+
+//#endregion -- FUNCTIONS --
