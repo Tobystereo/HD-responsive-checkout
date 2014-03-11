8f2d626debf013253d420d5619b7295d02b9ddce
mikekoser <mikek@hairdirect.com>
2014-03-11 11:27:32 -0400

    Toggle plugin

--- js/checkout.js
+++ js/checkout.js
@@ -1,4 +1,38 @@
-// Global Variables
+﻿/********** PLUGINS **********/
+(function ($) {
+	$.fn.toggleContainer = function (options) {
+		// Default options
+		var settings = $.extend({
+			content_element: undefined,
+			callback: undefined,
+			delay: 1,
+			show_class: "show",
+			hide_class: "hide",
+			firing_events: "click"
+		}, options);
+
+		if (settings.content_element !== undefined) {
+			this.on(settings.firing_events, function () {
+				var content_height;
+				if (settings.content_element.hasClass(settings.show_class)) {
+					settings.content_element.removeClass(settings.show_class).addClass(setting.hide_class);
+				}
+				else {
+					content_height = settings.content_element.height();
+					settings.content_element.addClass(settings.show_class);
+					setTimeout(function () {	
+						settings.content_element.css("height", content_height + "px");
+						if (settings.callback !== undefined) {
+							settings.callback(this);
+						}
+					}, settings.delay);
+				}
+			});
+		}
+		return this;
+	};
+}(jQuery));
+/********** GLOBAL VARIABLES **********/
 var $cta_bar,
 	$help_content,
 	$btnchangeaddress,
@@ -6,6 +40,8 @@ var $cta_bar,
 	$additional_addresses,
 	footertop,
 	absoluteClassName = "absolute";
+
+/********** FUNCTIONS **********/
 
 $(document).ready(function () {
 	SetGlobalVariables();
@@ -26,18 +62,20 @@ function WireEvents() {
 	$(window).on("load scroll", function (event) {
 		UpdateCtaBar();
 	});
-	$btnchangeaddress.on("click", function () {
-        var additional_addresses_height = $additional_addresses.height();
-		$additional_addresses.addClass("show");
-        setTimeout(function(){
-            $additional_addresses.css("height", additional_addresses_height + "px")
-        }, 1);
-
-		UpdateCtaBar();
+	$btnchangeaddress.toggleContainer({
+		content_element: $additional_addresses
+		//,callback: UpdateCtaBar
 	});
-	$btnchangeaddress.on("click", function () {
+	
+	//$btnchangeaddress.on("click", function () {
+	//	var additional_addresses_height = $additional_addresses.height();
+	//	$additional_addresses.addClass("show");
+	//	setTimeout(function () {
+	//		$additional_addresses.css("height", additional_addresses_height + "px")
+	//	}, 1);
 
-	});
+	//	UpdateCtaBar();
+	//});
 }
 
 function UpdateCtaBar() {
@@ -48,20 +86,29 @@ function UpdateCtaBar() {
 			ctabarheight = $cta_bar.height(),
 			footertop = $help_content.position().top;
 
-	if (scrollbottom >= footertop+ctabarheight) {
+	if (scrollbottom >= footertop + ctabarheight) {
 		// Remove the call to action bar's sticky behavior
-		if (!$cta_bar.hasClass(absoluteClassName)) {
-			$cta_bar.addClass(absoluteClassName).css({
-				"top": footertop + "px"
-			});
-		}
+		MakeCtaBarAbsolute(absoluteClassName, footertop);
 	}
 	else {
-		if ($cta_bar.hasClass(absoluteClassName)) {
-			// Add the sticky behavior to the call to action bar
-			$cta_bar.removeClass(absoluteClassName).css({
-				"top": "auto"
-			});
-		}
+		MakeCtaBarSticky(absoluteClassName);
+	}
+}
+
+function MakeCtaBarSticky(absoluteClassName) {
+	if ($cta_bar.hasClass(absoluteClassName)) {
+		// Add the sticky behavior to the call to action bar
+		$cta_bar.removeClass(absoluteClassName).css({
+			"top": "auto"
+		});
+	}
+}
+
+function MakeCtaBarAbsolute(absoluteClassName, footertop) {
+	// Remove the call to action bar's sticky behavior
+	if (!$cta_bar.hasClass(absoluteClassName)) {
+		$cta_bar.addClass(absoluteClassName).css({
+			"top": footertop + "px"
+		});
 	}
 }
