33c61c34759156fd18ac4fa2adfadd4cf9e8aff3
Tobias Treppmann <hello@tobystereo.com>
2014-03-14 13:48:02 -0400

    more button styles

--- js/checkout.js
+++ js/checkout.js
@@ -1,4 +1,4 @@
-﻿//#region -- PLUGINS --
+//#region -- PLUGINS -- aslkdjaskl dklasd jalks djaskld aksljd askldj lsk
 
 (function ($) {
 	var $html_body = $('html, body');
@@ -279,7 +279,7 @@ function BindEvents_NeedHelp(refreshSelector) {
 	if (refreshSelector) {
 		$need_help = $($need_help.selector);
 	}
-	$need_help.on("click", function (){ 
+	$need_help.on("click", function (){
 		$help_content.animatedScroll();
 	});
 }
